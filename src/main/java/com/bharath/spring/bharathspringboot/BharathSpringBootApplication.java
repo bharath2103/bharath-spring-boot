c82de2f44d93629b958b17f83fc34673f3eed8a2
Bharathkumar <bharath2103@gmail.com>
2017-10-18 22:49:09 +0530

    initial commit

---
+++ src/main/java/com/bharath/spring/bharathspringboot/BharathSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.bharath.spring.bharathspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharathSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharathSpringBootApplication.class, args);
+	}
+}
